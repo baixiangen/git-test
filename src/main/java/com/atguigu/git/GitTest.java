c48e4a2571a7eddf9f5b9286137353c528601f97
Xiangen <baixiangen@163.com>
2022-07-25 19:03:29 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello hadoop");
         System.out.println("test master");
         System.out.println("test hot-fix");
+        System.out.println("push test");
     }
 }
