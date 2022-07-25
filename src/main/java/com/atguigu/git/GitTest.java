42c145b7b807e4ca4b84cd5ba564ff72705f37f5
Xiangen <baixiangen@163.com>
2022-07-25 17:41:56 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello java");
+        System.out.println("hello hadoop");
     }
 }
